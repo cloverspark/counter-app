54df8c9b01fdfa6c61af82627160cd1be88b590b
cloverspark <alexbrenna1999@hotmail.com>
2021-06-15 22:16:30 -0700

    set upo dependent child class and reset button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,19 +9,44 @@ class Counters extends Component {
             {id:3, value:0},
             {id:4, value:0}
         ]
-      }
+    }
+
+    handleIncrement =(counter)=>{
+        
+        const counters =[...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index]={...counter};
+        counters[index].value++;
+        this.setState({counters})
+        
+    }
+    
+    handleReset = ()=>{
+        const counters = this.state.counters.map(c =>{
+            c.value = 0;
+            return c;
+        })
+        this.setState({counters})
+    }
+    
     handleDelete =(counterID)=>{
-       //console.log('was called',key) 
-       const counters = this.state.counters.filter(c => c.id !== counterID)
-       this.setState({counters})
+        //console.log('was called',key) 
+        const counters = this.state.counters.filter(c => c.id !== counterID)
+        this.setState({counters})
     }
     render() { 
         return (<div>
+            <button
+            onClick={ this.handleReset}
+            className="btn btn-primary btn-sm m-2"
+            >Reset
+            </button> 
             {this.state.counters.map(counter =>(
             <Counter 
             key={counter.id}
             //id={counter.id} 
-            onDelete={this.handleDelete} 
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement} 
             //value={counter.value}
             counter={counter}            
             />
