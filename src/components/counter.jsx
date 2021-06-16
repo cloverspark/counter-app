b6f999e3a920c6b888983a599f66995cd2b9c48c
cloverspark <alexbrenna1999@hotmail.com>
2021-06-15 22:28:23 -0700

    set up using app and changed tree

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value,
+    // state = {
+    //     value: this.props.counter.value,
         
-    }
+    // }
     // constructor(){ //use if you dont use arrow function with handleIncrement
     //     super();
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
-    handleIncrement = () =>{
+    // handleIncrement = () =>{
         
-        this.setState({value:this.state.value +1});
-    }
+    //     this.setState({value:this.state.value +1});
+    // }
     // styles ={
     //     fontSize: 25,
     //     fontSizeweight:"bold",
@@ -24,8 +24,10 @@ class Counter extends Component {
             //<span style={{fontSize:25}} className="badge  badge-primary m-2">{this.formatCount()}</span>
         return (
         <div>
-            <span className={this.getBadgeClasses()}>{this.formatValue()}</span> 
-            <button onClick={this.handleIncrement} 
+            <span className={this.getBadgeClasses()}>
+            {this.formatValue()}
+            </span> 
+            <button onClick={()=> this.props.onIncrement(this.props.counter)} 
             className="btn btn-secondary btn-sm">
             Increment
             </button>
@@ -39,12 +41,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatValue(){
-        const {value: value} = this.state;
+        const {value} = this.props.counter;
         return value === 0 ? 'zero' : value;
     }
 }
