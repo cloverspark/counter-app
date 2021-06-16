902d255f02c679d0b0d0fd484e314c3db1637ca6
cloverspark <alexbrenna1999@hotmail.com>
2021-06-15 21:28:23 -0700

    proper delete call

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,19 +3,20 @@ import Counter from './counter';
 class Counters extends Component {
     state = {
         counters:[
+            
             {id:1, value:0},
             {id:2, value:0},
             {id:3, value:0},
             {id:4, value:0}
         ]
       }
-      handleDelete =()=>{
-        console.log('was called') 
+      handleDelete =(key)=>{
+        console.log('was called',key) 
  }
     render() { 
         return (<div>
             {this.state.counters.map(counter =>(
-            <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value}/>
+            <Counter id={counter.id} onDelete={this.handleDelete} value={counter.value}/>
                
             
             ))}
